ac032168c74a1fb2dae46ea514c67a9e58bf5554
MUHAMMAD.AFAQUEKHAN <V4*eczrg>
2023-01-05 08:02:43 +0100

    Game variables initialization.

--- src/src/GamePanel.java
+++ src/src/GamePanel.java
@@ -2,8 +2,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREE_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREE_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int[] x_coordinates = new int[GAME_UNITS];
+    final int[] y_coordinates = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten = 0;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
     GamePanel(){
 
@@ -42,4 +61,10 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
 }
