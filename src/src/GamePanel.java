913c5651dce8a97da0f3839d7e07383e67094313
MUHAMMAD.AFAQUEKHAN <V4*eczrg>
2023-01-06 18:07:45 +0100

    Draw apple randomly.

--- src/src/GamePanel.java
+++ src/src/GamePanel.java
@@ -9,9 +9,9 @@ import java.util.Random;
 public class GamePanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 600;
-    static final int SCREE_HEIGHT = 600;
+    static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_WIDTH * SCREE_HEIGHT)/UNIT_SIZE;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY = 75;
     final int[] x_coordinates = new int[GAME_UNITS];
     final int[] y_coordinates = new int[GAME_UNITS];
@@ -26,7 +26,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     GamePanel(){
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREE_HEIGHT));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
@@ -34,15 +34,30 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void startGame(){
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
 
+    public void newApple(){
+        appleX = random.nextInt((int)SCREEN_WIDTH / UNIT_SIZE) * UNIT_SIZE;
+        appleY = random.nextInt((int)SCREEN_HEIGHT / UNIT_SIZE) * UNIT_SIZE;
     }
 
     public void paintComponent(Graphics g){
-
+        super.paintComponent(g);
+        draw(g);
     }
 
     public void draw(Graphics g){
-
+        g.setColor(Color.white);
+        for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+            g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i * UNIT_SIZE,  SCREEN_WIDTH, i * UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
     }
 
     public void move(){
